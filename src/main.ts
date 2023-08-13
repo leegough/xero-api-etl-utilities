5d1f8dd53dbc4664c48d085dd35fb222a019c74d
Lee Gough <leegough92@live.co.uk>
2023-08-13 18:16:47 +0100

    main

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { TokenSet, XeroClient } from "xero-node"
+import { refreshTokenSet, sendFileAttachmentsToXero, sendInvOrCRToXero, setActiveTenant } from "./apiFunctions"
+import storedToken from "./lib/tokenSet.json"
+import { createXeroDataObject } from "./createXeroDataObject"
+import { getLogPath, getTenantIndex, writeResponseLog } from "./helpers"
+
+async function main(entity: string) {
+  try {
+    const logPath = getLogPath(entity)
+    const tenantIndex = getTenantIndex(entity)
+
+    let xero = new XeroClient()
+    const tokenSet: TokenSet = new TokenSet(storedToken)
+
+    xero = await refreshTokenSet(tokenSet)
+    const activeTenantId = await setActiveTenant(tenantIndex, xero)
+
+    const { invoices, credits, fileAttachments } = await createXeroDataObject(logPath)
+    console.log("Date Objects created")
+
+    const { invRes, crRes } = await sendInvOrCRToXero(invoices, credits, xero, activeTenantId)
+    console.log("Date Objects sent to Xero API")
+
+
+    await writeResponseLog(invRes, crRes, logPath)
+    console.log("Xero Invoices created!")
+
+    await sendFileAttachmentsToXero(fileAttachments, xero, activeTenantId, logPath)
+    console.log("Attachments uploaded and attached")
+
+    process.exitCode = 0
+    return
+  } catch (err) {
+    console.error(err, null, 2)
+    process.exitCode = 1
+  }
+}
+
+main(process.argv[2])
